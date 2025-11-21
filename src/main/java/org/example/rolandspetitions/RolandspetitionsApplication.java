211770076eda27747b360a0208cfa8341cc3770b
Roland Vasarhelyi <r.vasarhelyi1@universityofgalway.ie>
2025-11-21 18:55:48 +0100

    Initial spring boot project

---
+++ src/main/java/org/example/rolandspetitions/RolandspetitionsApplication.java
@@ -0,0 +1,13 @@
+package org.example.rolandspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolandspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolandspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/rolandspetitions/RolandspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rolandspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolandspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
