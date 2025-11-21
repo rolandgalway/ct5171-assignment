358dd9d7069409c0004ce91accd0f84daf432b96
Roland Vasarhelyi <r.vasarhelyi1@universityofgalway.ie>
2025-11-21 19:23:25 +0100

    Created Petition model

--- src/main/java/org/example/rolandspetitions/model/Petition.java
+++ src/main/java/org/example/rolandspetitions/model/Petition.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Petition {
-
-    private int id;
     private String title;
     private String description;
-
     private List<Signature> signatures = new ArrayList<>();
 
-    public Petition(int id, String title, String description) {
-        this.id = id;
+    public Petition(String title, String description) {
         this.title = title;
         this.description = description;
     }
 
-    public int getId() { return id; }
-    public String getTitle() { return title; }
-    public String getDescription() { return description; }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 
     public List<Signature> getSignatures() {
         return signatures;
     }
 
     public void addSignature(Signature signature) {
-        this.signatures.add(signature);
+        signatures.add(signature);
     }
 }
